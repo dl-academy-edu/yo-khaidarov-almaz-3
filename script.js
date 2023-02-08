5b6f86e16ebb2befcba3649875aa9eb8e3705266
AlmazKhaydarov <114001062+AlmazKhaydarov@users.noreply.github.com>
2023-02-08 21:42:29 +0400

    1 домашка

---
+++ script.js
@@ -0,0 +1,21 @@
+let number = 75;                              // number
+let bigint = 3498364654378623987n;            // bigInt
+let str = 'some string';                      // string
+let isChecked = false;                        // boolean
+let item = null;                              // null
+let email = undefined;                        // undefined
+let coord = { x: 34, y: 24 };                 // object
+let id = Symbol('id');                        // symbol
+
+
+
+
+
+ console.log(Number(number), String(number), Boolean(number));
+ console.log(Number(bigint), String(bigint), Boolean(bigint)); 
+ console.log(Number(str), String(str), Boolean(str)); 
+ console.log(Number(isChecked), String(isChecked), Boolean(isChecked)); 
+ console.log(Number(item), String(item), Boolean(item)); 
+ console.log(Number(email), String(email), Boolean(email)); 
+ console.log(Number(coord), String(coord), Boolean(coord));
+ console.log(Number(id), String(id), Boolean(id));
