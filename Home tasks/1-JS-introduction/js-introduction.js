deb37b4b2507dae91721e5a77db5505467df0d55
AlmazKhaydarov <114001062+AlmazKhaydarov@users.noreply.github.com>
2023-02-19 10:44:48 +0400

    Все домашки дубль 2
    прошлый файл был недоделанный здесь я собрал все домашки

--- Home tasks/1-JS-introduction/js-introduction.js
+++ Home tasks/1-JS-introduction/js-introduction.js
@@ -1,22 +1,21 @@
-const myName = 'Samir';
-let myAge = 32;
-let myScores = 7777777n;
-const haveKid = true;
-let typeNull = null;
-let typeUndefined = undefined;
-let myData = {
-  name: 'Samir',
-  age: 32,
-  child: 'Vika',
-  musicBand: 'CDC',
-};
-let myMusic = Symbol('cdc');
+let number = 75;                              // number
+let bigint = 3498364654378623987n;            // bigInt
+let str = 'some string';                      // string
+let isChecked = false;                        // boolean
+let item = null;                              // null
+let email = undefined;                        // undefined
+let coord = { x: 34, y: 24 };                 // object
+let id = Symbol('id');                        // symbol
 
-console.log(Number(myName), String(myName), Boolean(myName));
-console.log(Number(myAge), String(myAge), Boolean(myAge));
-console.log(Number(myScores), String(myScores), Boolean(myScores));
-console.log(Number(haveKid), String(haveKid), Boolean(haveKid));
-console.log(Number(typeNull), String(typeNull), Boolean(typeNull));
-console.log(Number(typeUndefined), String(typeUndefined), Boolean(typeUndefined));
-console.log(Number(myData), String(myData), Boolean(myData));
-console.log(Number(myMusic), String(myMusic), Boolean(myMusic));
+
+
+
+
+ console.log(Number(number), String(number), Boolean(number));
+ console.log(Number(bigint), String(bigint), Boolean(bigint)); 
+ console.log(Number(str), String(str), Boolean(str)); 
+ console.log(Number(isChecked), String(isChecked), Boolean(isChecked)); 
+ console.log(Number(item), String(item), Boolean(item)); 
+ console.log(Number(email), String(email), Boolean(email)); 
+ console.log(Number(coord), String(coord), Boolean(coord));
+ console.log(Number(id), String(id), Boolean(id));
